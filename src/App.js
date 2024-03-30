eb2978c18e6d748a594a2c3bd50dd6316eb536ef
Huỳnh Vĩnh Tiến <dmx031117@gmail.om>
2024-03-30 20:36:34 +0700

    Tien push Hook useState useEffect

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://hvtienprotv84.github.io/MyWebsite/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Huỳnh Vĩnh Tiến - Hook - useState
+        </a>
+        <Example />
+      </header>
+    </div>
+  );
+}
+function Example() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+  return (
+    <div>
+      <p className="clicked">Bạn Đã Nhấp {count} Lần :)))</p>
+      <button className="clickme" onClick={() => setCount(count + 1)}>Click Me</button>
+      <button className="clickme" onClick={() => setCount(0)}>Reset Me</button>
+    </div>
+  );
+}
+export default App;
